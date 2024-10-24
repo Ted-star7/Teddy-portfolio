2106c3773f24987a5cff308a685e7fc2d4274f8a
Ted-star <kibuthuteddy@gmail.com>
2024-10-24 13:56:48 +0300

    final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class HomeComponent implements AfterViewInit {
   constructor(private titleService: Title, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
-    this.titleService.setTitle('Teddy Kibuthu- Home');
+    this.titleService.setTitle('Teddy Kibuthu - Home');
   }
 
   goToContact() {
@@ -28,33 +28,29 @@ export class HomeComponent implements AfterViewInit {
 
   // Function to animate progress bars from 0 to their percentage
   animateProgressBars() {
-    if (isPlatformBrowser(this.platformId)) {
-      const progressBars = document.querySelectorAll('.progress');
-      progressBars.forEach((bar) => {
-        const targetWidth = bar.getAttribute('data-target-width');
-        if (targetWidth !== null) {
-          let currentWidth = 0;
-          const targetWidthValue = parseInt(targetWidth, 10); // Convert to number
-          const interval = setInterval(() => {
-            if (currentWidth >= targetWidthValue) {
-              clearInterval(interval);
-            } else {
-              currentWidth++;
-              bar.setAttribute('style', `width: ${currentWidth}%`);
-            }
-          }, 5); // Adjust the speed here
-        }
-      });
-    }
+    const progressBars = document.querySelectorAll('.progress');
+    progressBars.forEach((bar) => {
+      const targetWidth = bar.getAttribute('data-target-width');
+      if (targetWidth) {
+        let currentWidth = 0;
+        const targetWidthValue = parseInt(targetWidth, 10); // Convert to number
+        const interval = setInterval(() => {
+          if (currentWidth >= targetWidthValue) {
+            clearInterval(interval);
+          } else {
+            currentWidth++;
+            bar.setAttribute('style', `width: ${currentWidth}%`);
+          }
+        }, 20); // Adjust the speed here for smoother transitions
+      }
+    });
   }
 
   // Function to add slide-in animation to "What I Can Do" section
   addSlideInAnimation() {
-    if (isPlatformBrowser(this.platformId)) {
-      const skillsSection = document.querySelector('.what-i-can-do');
-      if (skillsSection) {
-        skillsSection.classList.add('slide-in-left');
-      }
+    const skillsSection = document.querySelector('.what-i-can-do');
+    if (skillsSection) {
+      skillsSection.classList.add('slide-in-left');
     }
   }
 }
